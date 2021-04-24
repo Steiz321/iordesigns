2c702410ded7bb3d33795c2e7d659c371918d151
Steiz321 <fundamentdaiil@gmail.com>
2021-04-24 08:26:24 +0300

    visout forma

---
+++ assets/js/beforeafter.js
@@ -0,0 +1,12 @@
+/* before_after */
+$(function(){
+    $("#before__after1").twentytwenty();
+  });
+
+$(function(){
+    $("#before__after2").twentytwenty();
+  });
+
+$(function(){
+    $("#before__after3").twentytwenty();
+  });
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,53 @@
-/* before_after */
-$(function(){
-    $("#before__after1").twentytwenty();
-  });
+$(function() {
 
-$(function(){
-    $("#before__after2").twentytwenty();
-  });
+    var header = $("#header"),
+    introH = $("#intro").innerHeight(),
+    scrollOffset = $(window).scrollTop();
 
-$(function(){
-    $("#before__after3").twentytwenty();
-  });
+
+    /* Fixed Header */
+    /*checkScroll(scrollOffset);*/
+    
+    $(window).on("scroll", function() { 
+
+        scrollOffset = $(this).scrollTop();
+
+        checkScroll(scrollOffset);
+
+    });
+
+    function checkScroll() {
+
+
+        if(scrollOffset >= introH-100) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+    
+    /* Smooth Scroll */
+    $("[data-scroll]").on("click", function(event) {
+
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId =  $this.data('scroll'),
+            blockOffset = $(blockId).offset().top-100;
+ 
+        $("html, body").animate({
+            scrollTop: blockOffset
+        }, 500);
+    });
+
+
+    /* Collapse */
+    $("[data-collapse]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId =  $this.data('collapse');
+
+        $this.toggleClass("active");
+    });
+});
